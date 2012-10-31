93816775c98de0db80a58162764a4eea14f1906a
skwh <somekidwithhtml@gmail.com>
2012-10-30 23:25:56 -0600

    Heavily annotated and commented

--- algorithm.js
+++ algorithm.js
@@ -1,6 +1,6 @@
 function pathFinder(startNode,endNode,sourceGraph) {
-	var S = [];
-	var vS = sourceGraph.nodes;
+	var S = []; //the set of all visited nodes
+	var vS = sourceGraph.nodes; //the set of all unvisited nodes
 	function initialize_single_source(graph,node) { //initializes graph, starting point, and all other nodes
 		for (var v in graph.nodes) {
 			if (v instanceof Node) {
@@ -16,8 +16,8 @@ function pathFinder(startNode,endNode,sourceGraph) {
 			node2.pi = node1;	
 		}
 	}
-	function Adjacent(node) { //returns an array of adjacent nodes
-		if (!(node instanceof Node)) {
+	function Adjacent(node) { //returns an array of adjacent nodes and lines
+		if (!(node instanceof Node)) { //don't want any oops
 			return null;
 		}
 		var AdjacentNodes = [];
@@ -35,39 +35,43 @@ function pathFinder(startNode,endNode,sourceGraph) {
 				AdjacentNodes.push(neighborNode);	
 			}
 		}
-		return [AdjacentNodes,AdjacentLengths];
+		return [AdjacentNodes,AdjacentLengths]; //return nodes and lengths grouped into an array
 	}
 	initialize_single_source(sourceGraph,startNode);
-	while (vS.length != 0) {
-		vS = vS.sort(function(a,b) { return a.distance-b.distance; });
-		var u = vS.shift();
-		S.push(u);
-		u.visited = true;
-		var a = Adjacent(u);
-		var aN = a[0];
+	while (vS.length != 0) { //while there are still nodes to be sorted through
+		vS = vS.sort(function(a,b) { return a.distance-b.distance; }); //sort them by distance
+		var u = vS.shift(); //take the node with the least distance
+		S.push(u); //add this node to the visited set
+		u.visited = true; //set the internal flag to true
+		var a = Adjacent(u); //get the adjacent for this node
+		var aN = a[0]; //split the response into two variables
 		var aL = a[1];
 		for (var v=0;v<aN.length;v++) {
-			relax(u, aN[v], aL[v]);
+			relax(u, aN[v], aL[v]); //relax each connected node
 		}
 	}
-	var chNode = null;
+	/*
+		This loop modifies the Nodes such that their Pi (predescessor) represents the node that a path would travel through to
+		the given node. Now, to find the optimal path, backtrack through the S sorted array to the end node, and find the Pi's 
+		of each node.
+	*/
+	var chNode = null; //current node
 	for (var i=0;i<S.length;i++) {
-		if (S[i] == endNode) {
+		if (S[i] == endNode) { //find the end node
 			chNode = S[i];	
 		}
 	}
-	var path = [chNode];
-	var finished = false;
-	while (!finished) {
+	var path = [chNode]; //the path includes the start and end nodes
+	var finished = false; //random flag
+	while (!finished) { //backtrack through the Pi's
 		path.push(chNode.pi);
-		if (chNode.pi == startNode) {
+		if (chNode.pi == startNode) { //the algorithm is finished, this is the start node
 			finished = true;
-			path = path.reverse();
+			path = path.reverse(); //reverse the path so that it progresses from the start -> finish, rather than finish -> start
 			break;	
 		} else {
-			chNode = chNode.pi;	
+			chNode = chNode.pi;	//if this node is not the start node, get the next Pi
 		}
 	}
-	return path;
+	return path; //return the final path
 }
-//
