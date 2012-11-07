0d6213583b9994e45015dfe3aa268b9a63d9824d
skwh <somekidwithhtml@gmail.com>
2012-11-06 21:43:59 -0700

    Added beginning of algorithm

--- cSKWDA/cSKWDA/cSKWDA.cpp
+++ cSKWDA/cSKWDA/cSKWDA.cpp
@@ -1,11 +1,20 @@
 // cSKWDA.cpp : Defines the entry point for the console application.
 //
-
 #include "stdafx.h"
+#include <iostream>
+#include "Graph.h"
+#include "Edge.h"
+#include "Node.h"
+#include <vector>
 
-
-int _tmain(int argc, _TCHAR* argv[])
-{
+std::vector<Node> algorithm(Node, Node, Graph);
+int _tmain(int argc, _TCHAR* argv[]) {
+	
 	return 0;
 }
+std::vector<Node> algorithm(Node startNode, Node endNode, Graph g) {
+	std::vector<Node> Path;
+	std::vector<Node> VS;
+	std::vector<Node> S;
 
+}
