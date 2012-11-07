f90ace873ebab457c819c2fc8b6863795576317e
skwh <somekidwithhtml@gmail.com>
2012-11-06 21:45:27 -0700

    Fixed N1, N2, added methods

--- cSKWDA/cSKWDA/Edge.h
+++ cSKWDA/cSKWDA/Edge.h
@@ -2,10 +2,16 @@
 #include "Node.h"
 class Edge {
 	int v;
-	Node n1(void);
-	Node n2(void);
+	Node* n1;
+	Node* n2;
 public:
-	Edge(void);
+	Edge(Node n1, Node n2, int length);
 	~Edge(void);
+	int getV();
+	void setV(int d);
+	Node getN1();
+	void setN1(Node n);
+	Node getN2();
+	void setN2(Node n);
 };
 
