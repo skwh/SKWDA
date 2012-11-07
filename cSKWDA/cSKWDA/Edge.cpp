e8a5d3c3689a2b6b5e6478e508a8511f9a425bca
skwh <somekidwithhtml@gmail.com>
2012-11-06 21:45:08 -0700

    Added Edge methods

--- cSKWDA/cSKWDA/Edge.cpp
+++ cSKWDA/cSKWDA/Edge.cpp
@@ -1,9 +1,30 @@
 #include "StdAfx.h"
 #include "Edge.h"
 
-Edge::Edge(void) {
+Edge::Edge(Node N1, Node N2, int length) {
+	*n1 = N1;
+	*n2 = N2;
+	v = length;
+	n1->setAsEdge(this);
+	n2->setAsEdge(this);
 }
+Edge::~Edge(void) {}
 
-
-Edge::~Edge(void) {
+int Edge::getV() {
+	return v;
+}
+void Edge::setV(int d) {
+	v = d;
+}
+Node Edge::getN1() {
+	return *n1;
+}
+void Edge::setN1(Node n) {
+	*n1 = n;
+}
+Node Edge::getN2() {
+	return *n2;
+}
+void Edge::setN2(Node n) {
+	*n2 = n;
 }
